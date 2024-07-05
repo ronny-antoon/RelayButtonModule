3b02959044a1d14b7dedb35cba23744b0097810e
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2024-07-05 12:51:26 +0300

    ..

--- main/main.cpp
+++ main/main.cpp
@@ -1,15 +1,22 @@
-#include <unity.h>
-#include <unity_fixture.h>
-
+// #include <unity.h>
+// #include <unity_fixture.h>
 #include "ButtonModule.test.hpp"
 #include "RelayModule.test.hpp"
+#include <esp_log.h>
 
 extern "C" void app_main()
 {
-    UNITY_BEGIN();
+    // UNITY_BEGIN();
 
-    RUN_TEST_GROUP(ButtonModule_test);
-    RUN_TEST_GROUP(RelayModule_test);
+    // RUN_TEST_GROUP(ButtonModule_test);
+    // RUN_TEST_GROUP(RelayModule_test);
 
-    UNITY_END();
+    // UNITY_END();
+
+    ButtonModule * button = new ButtonModule(5,1,1000,200);
+    button->setSinglePressCallback([](void * param) { ESP_LOGI("Button", "Single press callback"); }, nullptr);
+
+    button->setDoublePressCallback([](void * param) { ESP_LOGI("Button", "Double press callback"); }, nullptr);
+
+    button->setLongPressCallback([](void * param) { ESP_LOGI("Button", "Long press callback"); }, nullptr);
 }
